27a7b1a9e64b85a9a8dbb469d10685c507147261
Diego Quintela <diegolucasq@gmail.com>
2022-01-14 10:51:03 -0500

    added functionality to cart buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+  const cartShowHandler = (e) => {
+    setCartIsShown(true);
+  };
+
   return (
     <Fragment>
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onCartShow={cartShowHandler} />
       <main>
         <Meals />
       </main>
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -10,7 +10,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>food order</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onCartShow} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="table served with food" />
