f3a6616d12354da62da11ef09c158aef00631eb0
Diego Quintela <diegolucasq@gmail.com>
2022-01-14 11:02:42 -0500

    added context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/cardProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -20,13 +21,13 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onCartShow={cartShowHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
