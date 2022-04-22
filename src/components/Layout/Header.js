549b1ae0f470d981c684fd2c20e6573d09d8ca15
Diego Quintela <diegolucasq@gmail.com>
2022-04-22 10:19:08 -0400

    small fix

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,18 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
 
-import HeaderCartButton from "./HeaderCartButton";
+import HeaderCartButton from './HeaderCartButton';
 
-import mealsImage from "../../assets/meals.jpeg";
-import classes from "./Header.module.css";
+import mealsImage from '../../assets/meals.jpeg';
+import classes from './Header.module.css';
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>food order</h1>
+        <h1>Mahlzeit</h1>
         <HeaderCartButton onClick={props.onCartShow} />
       </header>
-      <div className={classes["main-image"]}>
+      <div className={classes['main-image']}>
         <img src={mealsImage} alt="table served with food" />
       </div>
     </Fragment>
--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,16 +1,13 @@
-import classes from "./MealsSummary.module.css";
+import classes from './MealsSummary.module.css';
 
 const MealsSummary = () => {
   return (
     <section className={classes.summary}>
       <h2>food delivered to you</h2>
+      <p>Choose your favorite meal from our broad selection</p>
       <p>
-        choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home
-      </p>
-      <p>
-        all meals are cooked with high-quality ingredients, just-in-time and of
-        course by experienced chefs!
+        All meals are cooked with high-quality ingredients provided by local
+        farmers!
       </p>
     </section>
   );
